7d4666c8f13ff73462d192404b0e521fe17e9b9f
ray850301 <ray850301@gmail.com>
2017-10-24 00:31:44 +0800

    HW2_1
    大概這樣

--- HW2.java
+++ HW2.java
@@ -1,9 +1,10 @@
-import java.util.ArrayList;
+﻿import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 
 /**
  * @author TODO: please add student ID and name here
+ * 					資管四乙	B0344209          蕭瑞文
  * Try to write some comments for your codes (methods, 15 points)
  */
 public class HW2 {
@@ -73,17 +74,28 @@ class Deck{
 		cards=new ArrayList<Card>();
 		//1 Deck have 52 cards, https://en.wikipedia.org/wiki/Poker
 		//Hint: Use new Card(x,y) and 3 for loops to add card into deck
-		//Sample code start
-		//Card card=new Card(1,1); ->means new card as clubs ace
-		//cards.add(card);
-		//Sample code end
-
-	}	
+		for(int n=0; n<nDeck; n++) {						// 有N副牌
+			for(int i=1; i<=4; i++) {						// Definition: 1~4, Clubs=1, Diamonds=2, Hearts=3, Spades=4
+				for(int j=1; j<=13; j++){					// 1~13
+					Card card=new Card(i,j);	
+					cards.add(card);
+					}
+				}
+			}
+		}	
 	//TODO: Please implement the method to print all cards on screen (10 points)
 	public void printDeck(){
 		//Hint: print all items in ArrayList<Card> cards, 
 		//TODO: please implement and reuse printCard method in Card class (5 points)
-
+		int n=1;	// 從第一副牌開始
+		for(int i=0;i<cards.size();i++) {
+			if(i%52==0) {	// 每發52張牌就是下一副牌
+				System.out.print("\n第"+n+"副牌:\n");
+				n++;
+			}
+			cards.get(i).printCard();
+		}
+		System.out.println("");				//	純粹想換行
 	}
 	public ArrayList<Card> getAllCards(){
 		return cards;
@@ -93,8 +105,8 @@ class Deck{
  * Description: TODO: please add description here
  */
 class Card{
-	private int suit; //Definition: 1~4, Clubs=1, Diamonds=2, Hearts=3, Spades=4
-	private int rank; //1~13
+	private int suit; //Definition: 1~4, ♣ Clubs=1, ♦ Diamonds=2, ♥ Hearts=3, ♠ Spades=4
+	private int rank; //1~13	
 	/**
 	 * @param s suit
 	 * @param r rank
@@ -105,9 +117,128 @@ class Card{
 	}	
 	//TODO: 1. Please implement the printCard method (20 points, 10 for suit, 10 for rank)
 	public void printCard(){
-		//Hint: print (System.out.println) card as suit,rank, for example: print 1,1 as Clubs Ace
-
-	}
+		// 轉換後列印
+		String su="";       // 花色  String Type
+		String ra="";		// 點數  String Type
+		switch (suit) {
+        case 4:
+        	su="♠";		// Spades 黑桃
+        	switch (rank){
+        	case 13:
+        		ra="K";
+        		System.out.print("("+su+" "+ra+") ");
+        		//					花色		點數
+        		break;
+        	case 12:
+        		ra="Q";
+        		System.out.print("("+su+" "+ra+") ");
+        		//					花色		點數
+        		break;
+        	case 11:
+        		ra="J";
+        		System.out.print("("+su+" "+ra+") ");
+        		//					花色		點數
+        		break;
+        	case 1:
+        		ra="A";
+        		System.out.print("("+su+" "+ra+") ");
+        		//					花色		點數
+        		break;
+        	default:
+        		System.out.print("("+su+" "+getRank()+") ");
+        		//					花色			點數
+        		break;
+        		}
+        	break;
+        case 3:
+        	su="♥";		// Hearts 愛心
+        	switch (rank){
+        	case 13:
+        		ra="K";
+        		System.out.print("("+su+" "+ra+") ");
+        		//					花色		點數
+        		break;
+        	case 12:
+        		ra="Q";
+        		System.out.print("("+su+" "+ra+") ");
+        		//					花色		點數
+        		break;
+        	case 11:
+        		ra="J";
+        		System.out.print("("+su+" "+ra+") ");
+        		//					花色		點數
+        		break;
+        	case 1:
+        		ra="A";
+        		System.out.print("("+su+" "+ra+") ");
+        		//					花色		點數
+        		break;
+        	default:
+        		System.out.print("("+su+" "+getRank()+") ");
+        		//					花色			點數
+        		break;
+        		}
+        	break;
+        case 2:
+        	su="♦";		// Diamonds 方塊
+        	switch (rank){
+        	case 13:
+        		ra="K";
+        		System.out.print("("+su+" "+ra+") ");
+        		//					花色		點數
+        		break;
+        	case 12:
+        		ra="Q";
+        		System.out.print("("+su+" "+ra+") ");
+        		//					花色		點數
+        		break;
+        	case 11:
+        		ra="J";
+        		System.out.print("("+su+" "+ra+") ");
+        		//					花色		點數
+        		break;
+        	case 1:
+        		ra="A";
+        		System.out.print("("+su+" "+ra+") ");
+        		//					花色		點數
+        		break;
+        	default:
+        		System.out.print("("+su+" "+getRank()+") ");
+        		//					花色			點數
+        		break;
+        		}
+        	break;
+        case 1:
+        	su="♣";		// Clubs 梅花
+        	switch (rank){
+        	case 13:
+        		ra="K";
+        		System.out.print("("+su+" "+ra+") ");
+        		//					花色		點數
+        		break;
+        	case 12:
+        		ra="Q";
+        		System.out.print("("+su+" "+ra+") ");
+        		//					花色		點數
+        		break;
+        	case 11:
+        		ra="J";
+        		System.out.print("("+su+" "+ra+") ");
+        		//					花色		點數
+        		break;
+        	case 1:
+        		ra="A";
+        		System.out.print("("+su+" "+ra+") ");
+        		//					花色		點數
+        		break;
+        	default:
+        		System.out.print("("+su+" "+getRank()+") ");
+        		//					花色			點數
+        		break;
+        		}
+        	break;
+        	}
+		}
 	public int getSuit(){
 		return suit;
 	}
